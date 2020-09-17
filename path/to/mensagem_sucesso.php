90ace5e74d3bee232e59c25221f92ac3489dd70c
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-17 00:20:36 -0300

    Initial commit

---
+++ path/to/mensagem_sucesso.php
@@ -0,0 +1,50 @@
+<?php
+
+$mensagem_de_sucesso =
+"2018-10-10 19:34:37 SERVER -&gt; CLIENT: 220-ruby.svrdedicado.org ESMTP Exim 4.91 #1 Wed, 10 Oct 2018 16:34:39 -0300 220-We do not authorize the use of this system to transport unsolicited, 220 and/or bulk e-mail.<br>
+2018-10-10 19:34:37 CLIENT -&gt; SERVER: EHLO localhost<br>
+2018-10-10 19:34:37 SERVER -&gt; CLIENT: 250-ruby.svrdedicado.org Hello localhost [191.35.206.17]250-SIZE 52428800250-8BITMIME250-PIPELINING250-AUTH PLAIN LOGIN250 HELP<br>
+2018-10-10 19:34:37 CLIENT -&gt; SERVER: AUTH LOGIN<br>
+2018-10-10 19:34:37 SERVER -&gt; CLIENT: 334 VXNlcm5hbWU6<br>
+2018-10-10 19:34:37 CLIENT -&gt; SERVER: &lt;credentials hidden&gt;<br>
+2018-10-10 19:34:38 SERVER -&gt; CLIENT: 334 UGFzc3dvcmQ6<br>
+2018-10-10 19:34:38 CLIENT -&gt; SERVER: &lt;credentials hidden&gt;<br>
+2018-10-10 19:34:38 SERVER -&gt; CLIENT: 235 Authentication succeeded<br>
+2018-10-10 19:34:38 CLIENT -&gt; SERVER: MAIL FROM:&lt;formulario_site@rhaiters.com.br&gt;<br>
+2018-10-10 19:34:38 SERVER -&gt; CLIENT: 250 OK<br>
+2018-10-10 19:34:38 CLIENT -&gt; SERVER: RCPT TO:&lt;lab@rhaiters.com.br&gt;<br>
+2018-10-10 19:34:38 SERVER -&gt; CLIENT: 250 Accepted<br>
+2018-10-10 19:34:38 CLIENT -&gt; SERVER: DATA<br>
+2018-10-10 19:34:39 SERVER -&gt; CLIENT: 354 Enter message, ending with &quot;.&quot; on a line by itself<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Date: Wed, 10 Oct 2018 21:34:36 +0200<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: To: lab@rhaiters.com.br<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: From: Formulario Site &lt;formulario_site@rhaiters.com.br&gt;<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Subject: Mensagem de contato via site<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Message-ID: &lt;YZjgTxF4tDpjOyGY6LbGXFqRyzFTzxZhsUQLIITgPfI@localhost&gt;<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: X-Mailer: PHPMailer 6.0.5 (https://github.com/PHPMailer/PHPMailer)<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: MIME-Version: 1.0<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Content-Type: multipart/alternative;<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: 	boundary=&quot;b1_YZjgTxF4tDpjOyGY6LbGXFqRyzFTzxZhsUQLIITgPfI&quot;<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Content-Transfer-Encoding: 8bit<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: This is a multi-part message in MIME format.<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: --b1_YZjgTxF4tDpjOyGY6LbGXFqRyzFTzxZhsUQLIITgPfI<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Content-Type: text/plain; charset=us-ascii<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: This is the body in plain text for non-HTML mail clients<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: --b1_YZjgTxF4tDpjOyGY6LbGXFqRyzFTzxZhsUQLIITgPfI<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: Content-Type: text/html; charset=us-ascii<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: This is the HTML message body &lt;b&gt;in bold!&lt;/b&gt;<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: --b1_YZjgTxF4tDpjOyGY6LbGXFqRyzFTzxZhsUQLIITgPfI--<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: <br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: .<br>
+2018-10-10 19:34:39 SERVER -&gt; CLIENT: 250 OK id=1gAKFl-0001Jp-D9<br>
+2018-10-10 19:34:39 CLIENT -&gt; SERVER: QUIT<br>
+2018-10-10 19:34:39 SERVER -&gt; CLIENT: 221 ruby.svrdedicado.org closing connection<br>
+Mensagem enviada com sucesso.";
+
+?>
